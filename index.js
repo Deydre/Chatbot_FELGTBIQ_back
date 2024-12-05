89bdf5e4cccfb6e4cd17bf1abdf116fe18a96466
RubenC2 <rubenct1993@gmail.com>
2024-12-05 15:55:32 +0100

    try1

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const entradasRouter = require('./routes/entradasRoutes');
 app.use(express.json());
 
 // app.use rutas
+// GET http://localhost:3000/ --> Ruta /. La principal
+app.get("/", (req, res) => {
+  res.send("Hello World!. Welcome to Backend");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
