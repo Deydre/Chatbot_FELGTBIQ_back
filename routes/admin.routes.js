2b7d3d9e06f1aaf170e929533690ee84341a7885
RubenC2 <rubenct1993@gmail.com>
2024-12-07 21:49:20 +0100

    fixedErrorData

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -7,13 +7,11 @@ const adminController = require("../controllers/admin.controller");
 const router = express.Router();
 const {userDataValidateChainMethod} = require('../validation/admin.validation');
 
-// Poner rutas
-router.post('/login', userDataValidateChainMethod, adminController.login);
-// router.post('/login', adminController.login);
 
+router.post('/login', userDataValidateChainMethod, adminController.login);
 router.get('/logout', adminController.logout);
 router.get('/me', getAccessToken, decodeToken, adminController.getAdminByEmail);
-// router.get('/me', adminController.getAdminByEmail);
+
 
 
 module.exports = router;
