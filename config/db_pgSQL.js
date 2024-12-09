ef38480e2d6b977ce45f5f33c183c5b13e451d16
Hans <alvarohansorellana@gmail.com>
2024-12-09 18:04:04 +0100

    aws cert

--- config/db_pgSQL.js
+++ config/db_pgSQL.js
@@ -1,23 +1,24 @@
+const fs = require('fs');
 require('dotenv').config();
-const pg = require('pg');
-const { Pool } = pg;
-//local
-// let localPoolConfig = {
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE,
-// };
+const { Pool } = require('pg');
+
+// Debugging the path for the certificate
+console.log('Looking for CA file at:', __dirname + '/../certs/eu-west-1-bundle.pem');
+
 let localPoolConfig = {
     user: process.env.DB_USER_AWS,
     password: process.env.DB_PASSWORD_AWS,
     host: process.env.DB_HOST_AWS,
     port: process.env.DB_PORT_AWS,
     database: process.env.DB_DATABASE_AWS,
-    // ssl: true
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(__dirname + '/certs/eu-west-1-bundle.pem').toString(), // Ensure this path is correct
+    },
 };
 
+// Initialize the database pool
 const pool = new Pool(localPoolConfig);
 
+// Export the pool for use in other parts of the application
 module.exports = pool;
